e9beb20b395c1904f0aa98e7b6b736d3e67ddb7b
zts <247931368@qq.com>
2019-03-30 11:42:50 +0800

    新增：首页的实现

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,7 +1,10 @@
 'use strict'
+require('./index.css')
 require('../../page/common/nav/index.js')
 require('../../page/common/header/index.js')
+require('../../util/slider/index.js')
 var navSide = require('../../page/common/nav-side/index.js')
+var templateBanner = require('./banner.string')
 var _mm = require('../../util/util.js')
 
 navSide.init({
@@ -26,3 +29,17 @@ navSide.init({
 // }
 // console.log(_mm.renderHtml(html, data))
 
+$(function () {
+    // 渲染banner的html
+    var bannerHtml = _mm.renderHtml(templateBanner)
+    $('.banner-con').html(bannerHtml)
+    // 初始化banner
+    var $slider = $('.banner').unslider({
+        dots: true
+    })
+    // 前后按钮操作的事件绑定
+    $('.banner-con .banner-arrow').click(function () {
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next'
+        $slider.data('unslider')[forward]()
+    })
+})
